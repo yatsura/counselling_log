e212d115801a7bb01344e9749504987260f2ef7d
Paul Mitchell <paul.d.mitchell@ed.ac.uk>
2014-02-03 20:29:41 +0000

    Initial Rails 4 setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CounsellingLog::Application.config.secret_key_base = 'ab83c1455636a0451e8c91128f35a686bacb7e91bebc417e5c6298ec8f9f53d143bf210a236bf9217a1d371a5e8c0fc942da6b2de2d366911210240a1873afed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CounsellingLog::Application.config.session_store :cookie_store, key: '_counselling_log_session'
