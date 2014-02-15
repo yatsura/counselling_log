dac4899ec566003cce432b5d51edd064cca47c96
Paul Mitchell <paul.d.mitchell@ed.ac.uk>
2014-02-15 18:16:37 +0000

    Added basic client update

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,11 +1,37 @@
 class ClientsController < ApplicationController
+  before_filter :get_by_id, :only => [:edit, :update]
+  before_filter :get_new, :only => [:new]
 
+  def get_new
+  end
+
+  def get_by_id
+    @form_vars = Client.find(params[:id])
+  end
+  
   def index
     @grid = ClientsGrid.new(params[:clients_grid])
     @assets = @grid.assets.paginate(:page => params[:page], :per_page => 10)
   end
 
   def edit
+  end
+
+  def new
+  end
+
+  def update
+    respond_to do |format|
+      if @form_vars.update_attributes(client_params)
+        format.html { redirect_to(clients_path, :notice => t(:successful_form_model_update, :scope => :standard_forms, :resource_name => @form_vars.class.model_name.human))}
+      else
+        format.html { render :action => "edit"}
+      end
+    end
+  end
+
+  def client_params
+    params.require(:client).permit!
   end
 end
 
