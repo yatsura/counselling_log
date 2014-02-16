99477ee3f53bf44167ce3b7af48b78623e3b2918
Paul Mitchell <paul.d.mitchell@ed.ac.uk>
2014-02-16 17:25:08 +0000

    Counselling sessions controller

--- app/controllers/counselling_sessions_controller.rb
+++ app/controllers/counselling_sessions_controller.rb
@@ -1,4 +1,48 @@
 class CounsellingSessionsController < ApplicationController
+  before_filter :get_by_id, :only => [:edit, :update]
+  before_filter :get_new, :only => [:new, :create]
+
+  def get_new
+    @form_vars = CounsellingSession.new
+  end
+
+  def get_by_id
+    @form_vars = CounsellingSession.find(params[:id])
+  end
+  
+  def index
+    @grid = CounsellingSessionsGrid.new(params[:counselling_sessions_grid])
+    @assets = @grid.assets.paginate(:page => params[:page], :per_page => 10)
+  end
+
+  def edit
+  end
+
   def new
   end
+
+  def create
+    respond_to do |format|
+      if @form_vars.update_attributes(counselling_sessions_params)
+        format.html { redirect_to(counselling_sessions_path, :notice => t(:successful_form_model_create, :scope => :standard_forms, :resource_name => @form_vars.class.model_name.human))}
+      else
+        format.html { render :action => "new"}
+      end
+    end
+  end
+  
+  def update
+    respond_to do |format|
+      if @form_vars.update_attributes(counselling_sessions_params)
+        format.html { redirect_to(counselling_sessions_path, :notice => t(:successful_form_model_update, :scope => :standard_forms, :resource_name => @form_vars.class.model_name.human))}
+      else
+        format.html { render :action => "edit"}
+      end
+    end
+  end
+
+  def counselling_sessions_params
+    params.require(:counselling_sessions_params).permit!
+  end
 end
+
--- spec/features/clients/index_clients_spec.rb
+++ spec/features/clients/index_clients_spec.rb
@@ -3,20 +3,28 @@ require 'spec_helper'
 describe "Client list" do
   before :each do
     FactoryGirl.create :organisation
-    FactoryGirl.create :client_adult_male
+    @client = FactoryGirl.create :client_adult_male
   end
 
   it "edits a client" do
-    visit '/clients'
+    visit clients_path
 
     click_link 'Edit'
     expect(page).to have_content 'Edit Client'
   end
 
   it "can make a new client" do
-    visit '/clients'
+    visit clients_path
 
     click_link 'New'
     expect(page).to have_content 'New Client'
+  end
+
+  it "lists the client sessions" do
+    visit clients_path
+
+    click_link 'Sessions'
+    expect(page).to have_content 'Client Sessions'
+    expect(page).to have_content @client.code
   end
 end
