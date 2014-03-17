f00d3741d2c6cab173d1872939dabe05cde292c3
Paul Mitchell <paul.d.mitchell@ed.ac.uk>
2014-03-17 07:25:09 +0000

    Added clients link to index

--- app/grids/organisations_grid.rb
+++ app/grids/organisations_grid.rb
@@ -12,6 +12,10 @@ class OrganisationsGrid
   column(:telephone)
   column(:contact_name)
   column(:contact_email) # Change to email link
+  column(:clients, :header => 'Clients', :html => true) do |organisation|
+    link_to "Clients", organisation_clients_path(organisation)
+  end
+  
   column(:session, :header => 'Sessions', :html => true) do |organisation|
     link_to "Sessions", organisation_counselling_sessions_path(organisation)
   end
