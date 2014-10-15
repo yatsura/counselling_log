196f2c706b990e4bc7b4389c4474b854f087a722
Paul Mitchell <paul.d.mitchell@ed.ac.uk>
2014-10-15 07:31:19 +0100

    * Fixed clients
      The inherited resources controller helper as been removed
      all these changes from here will work on replacing the
      functionality

--- app/cells/contact_cell.rb
+++ app/cells/contact_cell.rb
@@ -2,7 +2,7 @@ class ContactCell < Cell::Rails
 
   def show(args)
     ctq = (args[:query_object] || So::ContactTimeQuery).new
-    st  = (args[:supervision_time] || Vo::CoscaAdultSupervisionTime).new
+    st  = (args[:supervision_time] || Vo::CoscaAdultSupervisionTime)
     @zone = args[:zone]
     @total_zone = ctq.total_time(@zone)
     @unsupervised_contact_time = ctq.unsupervised_contact_time(@zone)
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,16 +1,42 @@
 class ClientsController < ApplicationController
+  helper_method :resource_class, :parent, :resource, :new_resource_path, :collection_path
+
+  before_action :get_new, only: [:new, :create]
+
+  def get_new
+    @client = Client.new
+  end
+
+  def resource_class
+    Client
+  end
+
+  def parent
+    nil
+  end
+
+  def resource
+    @client
+  end
+
+  def new_resource_path
+    new_client_path
+  end
+
+  def collection_path
+    clients_path
+  end
+
   def index
     @grid = ClientsGrid.new(params[:clients_grid])
     @assets = @grid.assets.paginate(:page => params[:page], :per_page => 10)
   end
 
   def new
-    @client = Client.new
     @organisation = Organisation.find params[:organisation_id] if params.has_key?(:organisation_id)
   end
 
   def create
-    @client = Client.new
     if @client.update_attributes(build_resource_params)
       redirect_to clients_path, :notice => I18n.t(:notice, :scope => 'flash.actions.create', :resource_name => Client.name)
     else
