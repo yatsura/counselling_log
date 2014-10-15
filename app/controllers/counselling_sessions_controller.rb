07cd805f1e9a2f019b21cf8763fd94e21bfc3c78
Paul Mitchell <paul.d.mitchell@ed.ac.uk>
2014-10-15 08:13:08 +0100

    Organisations altered from inherited resources

--- app/controllers/counselling_sessions_controller.rb
+++ app/controllers/counselling_sessions_controller.rb
@@ -33,11 +33,10 @@ class CounsellingSessionsController < ApplicationController
   end
 
   def new
-    @counselling_session = CounsellingSession.new
+
   end
 
   def create
-    @counselling_session = CounsellingSession.new
     if @counselling_session.update_attributes(build_resource_params)
       redirect_to counselling_sessions_path, :notice => I18n.t(:notice, :scope => 'flash.actions.create', :resource_name => CounsellingSession.name)
     else
--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,14 +1,41 @@
 class OrganisationsController < ApplicationController
+  helper_method :resource_class, :parent, :resource, :new_resource_path, :collection_path
+
+  before_action :get_new, only: [:new, :create]
+
+  def get_new
+    @organisation = Organisation.new
+  end
+
+  def resource_class
+    Organisation
+  end
+
+  def parent
+    nil
+  end
+
+  def resource
+    @organisation
+  end
+
+  def new_resource_path
+    new_organisation_path
+  end
+
+  def collection_path
+    organisations_path
+  end
 
   def index
     @grid = OrganisationsGrid.new params[:organisations_grid]
     @assets = @grid.assets.paginate(:page => params[:page], :per_page => 10)
   end
-  
+
   def new
     @organisation = Organisation.new
-  end 
-  
+  end
+
   def create
     @organisation = Organisation.new
     if @organisation.update_attributes(build_resource_params)
@@ -17,11 +44,11 @@ class OrganisationsController < ApplicationController
       render "new"
     end
   end
-  
+
   def edit
     @organisation = Organisation.find params[:id]
   end
-  
+
   def update
     @organisation = Organisation.find params[:id]
     if @organisation.update_attributes(build_resource_params)
@@ -30,11 +57,11 @@ class OrganisationsController < ApplicationController
       render "edit"
     end
   end
-  
+
   def destroy
     @organisation = Organisation.find params[:id]
     @organisation.destroy
-    
+
     redirect_to supervisors_path, :notice => I18n.t(:notice, :scope => 'flash.actions.destroy', :resource_name => Organisation.name)
   end
 
