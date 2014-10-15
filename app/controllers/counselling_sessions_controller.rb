998f20ea7af9ce23b024aa6513f3dd461e859414
Paul Mitchell <paul.d.mitchell@ed.ac.uk>
2014-10-15 08:09:55 +0100

    Counselling altered from inherited resources

--- app/controllers/counselling_sessions_controller.rb
+++ app/controllers/counselling_sessions_controller.rb
@@ -1,4 +1,32 @@
 class CounsellingSessionsController < ApplicationController
+  helper_method :resource_class, :parent, :resource, :new_resource_path, :collection_path, :clients
+
+  before_action :get_new, only: [:new, :create]
+
+  def get_new
+    @counselling_session = CounsellingSession.new
+  end
+
+  def resource_class
+    CounsellingSession
+  end
+
+  def parent
+    nil
+  end
+
+  def resource
+    @counselling_session
+  end
+
+  def new_resource_path
+    new_counselling_session_path
+  end
+
+  def collection_path
+    counselling_sessions_path
+  end
+
   def index
     @grid = CounsellingSessionsGrid.new(params[:counselling_sessions_grid])
     @assets = @grid.assets.paginate(:page => params[:page], :per_page => 10)
@@ -6,8 +34,8 @@ class CounsellingSessionsController < ApplicationController
 
   def new
     @counselling_session = CounsellingSession.new
-  end 
-  
+  end
+
   def create
     @counselling_session = CounsellingSession.new
     if @counselling_session.update_attributes(build_resource_params)
@@ -16,11 +44,11 @@ class CounsellingSessionsController < ApplicationController
       render "new"
     end
   end
-  
+
   def edit
     @counselling_session = CounsellingSession.find params[:id]
   end
-  
+
   def update
     @counselling_session = CounsellingSession.find params[:id]
     if @counselling_session.update_attributes(build_resource_params)
@@ -29,15 +57,14 @@ class CounsellingSessionsController < ApplicationController
       render "edit"
     end
   end
-  
+
   def destroy
     @counselling_session = CounsellingSession.find params[:id]
     @counselling_session.destroy
-    
+
     redirect_to counselling_sessions_path, :notice => I18n.t(:notice, :scope => 'flash.actions.destroy', :resource_name => CounsellingSession.name)
   end
-  
-  # TODO discover why this is here
+
   def clients
     client_lookup = Proc.new { |n| [n.code,n.id] }
     if parent.is_a? Organisation
@@ -51,10 +78,9 @@ class CounsellingSessionsController < ApplicationController
       CounsellingSession.all.map(&client_lookup)
     end
   end
-  
+
   protected
   def build_resource_params
     params.require(:counselling_session).permit(:meetable_id, :zone, :date, :length, :notes, :billed)
   end
 end
-
