0da3d3f67e23b8fa0c35f5f722355228fea01296
Paul Mitchell <paul.d.mitchell@ed.ac.uk>
2014-10-16 20:44:25 +0100

    Changed vo.from_minutes to class method

--- spec/cells/contact_cell_spec.rb
+++ spec/cells/contact_cell_spec.rb
@@ -1,18 +1,17 @@
 require 'spec_helper'
 
 describe ContactCell do
-
   class SupervisionTimeMock
-    def from_minutes(args)
+    def self.from_minutes(args)
       "test"
     end
   end
-  
+
   class QueryObjectMock
     def total_time(args)
       600
-    end 
-    
+    end
+
     def unsupervised_contact_time(args)
       360
     end
@@ -28,12 +27,12 @@ describe ContactCell do
 
     it { should have_selector("span#total-time", :text => QueryObjectMock.new.total_time(nil)/60) }
     it { should have_selector("span#unsupervised-contact-time", :text => QueryObjectMock.new.unsupervised_contact_time(nil)/60) }
-    it { should have_selector("span#supervision-message", :text => SupervisionTimeMock.new.from_minutes(nil)) }
+    it { should have_selector("span#supervision-message", :text => SupervisionTimeMock.from_minutes(nil)) }
 
     context "rendering adult time" do
       it { should have_selector("legend", :text => "Adult") }
     end
-    
+
     context "rendering child time" do
       subject { render_cell(:contact, :show, :zone => "Child", :supervision_time => SupervisionTimeMock, :query_object => QueryObjectMock) }
 
