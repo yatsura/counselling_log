0befc3e1074fd2112a1717fb2114508fc94cdf97
Paul Mitchell <paul.d.mitchell@ed.ac.uk>
2014-11-06 16:39:42 +0000

    Client organisation_name test

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -10,4 +10,10 @@ describe Client, :type => :model do
   it "by default lists visible clients" do
     expect(Client.all.count).to eq(5)
   end
+
+  it "organisation_name is the client referral organisation name" do
+    @client = Client.first
+    @client.organisation = @organisation
+    expect(@client.organisation_name).to eq(@organisation.name)
+  end
 end
